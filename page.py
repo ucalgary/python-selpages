c589e08f9823b3f857b09fbbcaa27cb90646c04c
King Chung Huang <kinghuang@mac.com>
2017-06-15 17:22:44 -0600

    Initialize Element instances with locator
    Initialize Element instances with one or more locators. The locators
    are used to find the specified element within a page. If multiple
    locator argument are specified, then they are tried in order until an
    element is found.

--- page.py
+++ page.py
@@ -10,5 +10,26 @@ class Page(object):
         return self._webdriver
 
 
+_LOCATORS = {
+    'id': By.ID,
+    'name': By.NAME,
+    'tag': By.TAG_NAME,
+    'class_name': By.CLASS_NAME,
+    'link': By.LINK_TEXT,
+    'partial_link': By.PARTIAL_LINK_TEXT,
+    'selector': By.CSS_SELECTOR,
+    'xpath': By.XPATH
+}
+
+
 class Element(object):
-    pass
+
+    def __init__(self, id=None, name=None, tag=None, class_name=None, link_text=None, partial_link_text=None,
+                 css=None, xpath=None):
+        args = locals()
+        self._locators = [(_LOCATORS[arg], args[arg]) for arg, by in _LOCATORS.items() if args.get(arg)]
+        self._multiple = multiple
+
+    @property
+    def locators(self):
+        return self._locators
