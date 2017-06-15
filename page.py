d20a0f768579244afe3bb3f265b89380e465b613
King Chung Huang <kinghuang@mac.com>
2017-06-15 17:00:36 -0600

    Initialize Page instances with a webdriver
    Initialize Page instance with a Selenium webdriver instance. The
    webdriver provides access to a browser context with which to load web
    pages.

--- page.py
+++ page.py
@@ -1,5 +1,13 @@
 class Page(object):
-    pass
+
+    def __init__(self, webdriver):
+        if webdriver is None:
+            raise ValueError('webdriver must not be None')
+        self._webdriver = webdriver
+
+    @property
+    def webdriver(self):
+        return self._webdriver
 
 
 class Element(object):
